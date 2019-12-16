b46800d781c05cda39e3c122db0399054ced81b4
Stephen Hodgson <hodgson.designs@gmail.com>
2019-12-16 14:12:10 -0500

    updated namespaces for inspectors

--- XRTK.Oculus/Packages/com.xrtk.oculus/Inspectors/OculusGoControllerMappingProfileInspector.cs
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Inspectors/OculusGoControllerMappingProfileInspector.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEditor;
-using XRTK.Inspectors.Profiles;
+using XRTK.Inspectors.Profiles.InputSystem.Controllers;
 using XRTK.Oculus.Profiles;
 
 namespace XRTK.Oculus.Inspectors
