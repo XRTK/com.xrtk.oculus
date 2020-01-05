6c2562dcdc07cd7a6e4bee5e1e6e049307b78f53
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-01-05 14:55:25 +0000

    Adds the Validation logic to the oculus platform when loaded

---
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Editor/ValidateOculusControllerMappings.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using UnityEngine;
+using XRTK.Oculus.Controllers;
+using XRTK.Utilities.Editor;
+
+namespace XRTK.Oculus
+{
+    [InitializeOnLoad]
+    public class ValidateOculusControllerMappings
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        static ValidateOculusControllerMappings()
+        {
+            if (!Application.isBatchMode)
+            {
+                EditorApplication.delayCall += CheckControllerMappings;
+            }
+        }
+
+        public static void CheckControllerMappings()
+        {
+            bool result = false;
+            // Test if there is an Native Oculus controller profile registered
+            if (ValidateConfiguration.ValidateControllerProfiles(new[] { typeof(OculusTouchController) }))
+            {
+                //if there is profile, also check if there are any compatible mappings.  Left tests both Left and Right
+                result = ValidateConfiguration.ValidateControllerMappings(new[] { typeof(OculusTouchController) }, XRTK.Definitions.Utilities.Handedness.Left);
+            }
+        }
+    }
+}
