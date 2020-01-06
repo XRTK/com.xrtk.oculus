c999474b9afaabaf300234fcb53877f945a01b3f
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-01-06 15:37:50 +0000

    Updated Array to a static reference

--- XRTK.Oculus/Packages/com.xrtk.oculus/Editor/ValidateOculusControllerMappings.cs
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Editor/ValidateOculusControllerMappings.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using UnityEditor;
 using UnityEngine;
 using XRTK.Oculus.Controllers;
@@ -11,6 +12,8 @@ namespace XRTK.Oculus
     [InitializeOnLoad]
     public class ValidateOculusControllerMappings
     {
+        private static Type[] controllerTypes = new[] { typeof(OculusTouchController) };
+        
         /// <summary>
         /// Constructor.
         /// </summary>
@@ -26,7 +29,7 @@ namespace XRTK.Oculus
         {
             bool result = false;
             // Test if there is an Native Oculus controller profile registered
-            if (ValidateConfiguration.ValidateControllerProfiles(new[] { typeof(OculusTouchController) }))
+            if (ValidateConfiguration.ValidateControllerProfiles(controllerTypes))
             {
                 //if there is profile, also check if there are any compatible mappings.  Left tests both Left and Right
                 result = ValidateConfiguration.ValidateControllerMappings(new[] { typeof(OculusTouchController) }, XRTK.Definitions.Utilities.Handedness.Left);
