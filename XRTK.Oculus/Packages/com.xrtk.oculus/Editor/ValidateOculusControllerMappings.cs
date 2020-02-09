d7d9433109776842b4267b74b44cb734134e7241
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-02-09 12:56:50 +0000

    Added Data Provider validation checking

--- XRTK.Oculus/Packages/com.xrtk.oculus/Editor/ValidateOculusControllerMappings.cs
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Editor/ValidateOculusControllerMappings.cs
@@ -4,6 +4,8 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using XRTK.Definitions.InputSystem;
+using XRTK.Definitions.Utilities;
 using XRTK.Oculus.Controllers;
 using XRTK.Utilities;
 
@@ -12,6 +14,8 @@ namespace XRTK.Oculus
     [InitializeOnLoad]
     public class ValidateOculusControllerMappings
     {
+        private static Type[] dataProviderTypes = new[] { typeof(OculusControllerDataProvider) };
+        private static ControllerDataProviderConfiguration[] dataProviderConfiguration = new [] {new ControllerDataProviderConfiguration(typeof(OculusControllerDataProvider), "Oculus Controller Data Provider", 2, SupportedPlatforms.WindowsStandalone | SupportedPlatforms.Editor, null) };
         private static Type[] controllerTypes = new[] { typeof(OculusTouchController) };
 
         /// <summary>
@@ -28,12 +32,16 @@ namespace XRTK.Oculus
         public static void CheckControllerMappings()
         {
             bool result = false;
-            // Test if there is an Native Oculus controller profile registered
-            if (ValidateConfiguration.ValidateControllerProfiles(controllerTypes))
+            if (ValidateConfiguration.ValidateDataProviders(dataProviderTypes, dataProviderConfiguration))
             {
-                //if there is profile, also check if there are any compatible mappings.  Left tests both Left and Right
-                result = ValidateConfiguration.ValidateControllerMappings(new[] { typeof(OculusTouchController) }, XRTK.Definitions.Utilities.Handedness.Left);
+                // Test if there is an Native Oculus controller profile registered
+                if (ValidateConfiguration.ValidateControllerProfiles(controllerTypes))
+                {
+                    //if there is profile, also check if there are any compatible mappings.  Left tests both Left and Right
+                    result = ValidateConfiguration.ValidateControllerMappings(new[] { typeof(OculusTouchController) }, XRTK.Definitions.Utilities.Handedness.Left);
+                }
             }
+
         }
     }
 }
