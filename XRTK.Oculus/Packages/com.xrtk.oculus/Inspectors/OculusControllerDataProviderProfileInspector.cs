6e5a3322af136ad86a334da37a5a99910bfe6820
Dino Fejzagic <dino.f@live.de>
2020-04-02 12:18:04 +0200

    Introduce OculusControllerDataProviderProfile

---
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Inspectors/OculusControllerDataProviderProfileInspector.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using XRTK.Inspectors.Profiles;
+using XRTK.Inspectors.Utilities;
+using XRTK.Oculus.Profiles;
+
+namespace XRTK.Oculus.Inspectors
+{
+    /// <summary>
+    /// Default inspector for <see cref="OculusControllerDataProviderProfile"/>.
+    /// </summary>
+    [CustomEditor(typeof(OculusControllerDataProviderProfile))]
+    public class OculusControllerDataProviderProfileInspector : BaseMixedRealityProfileInspector
+    {
+        // Global settings overrides
+        private SerializedProperty handMeshingEnabled;
+        private SerializedProperty handRayType;
+        private SerializedProperty handPhysicsEnabled;
+        private SerializedProperty useTriggers;
+        private SerializedProperty boundsMode;
+
+        // Oculus specific settings
+        private SerializedProperty minConfidenceRequired;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            handMeshingEnabled = serializedObject.FindProperty(nameof(handMeshingEnabled));
+            handRayType = serializedObject.FindProperty(nameof(handRayType));
+            handPhysicsEnabled = serializedObject.FindProperty(nameof(handPhysicsEnabled));
+            useTriggers = serializedObject.FindProperty(nameof(useTriggers));
+            boundsMode = serializedObject.FindProperty(nameof(boundsMode));
+
+            minConfidenceRequired = serializedObject.FindProperty(nameof(minConfidenceRequired));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RenderHeader();
+
+            ThisProfile.CheckProfileLock();
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(handMeshingEnabled);
+            EditorGUILayout.PropertyField(handRayType);
+            EditorGUILayout.PropertyField(handPhysicsEnabled);
+            EditorGUILayout.PropertyField(useTriggers);
+            EditorGUILayout.PropertyField(boundsMode);
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(minConfidenceRequired);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
