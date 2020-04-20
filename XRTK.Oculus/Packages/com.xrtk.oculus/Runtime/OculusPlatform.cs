0ee9f162ca27a945754c7f216ed3e42799adac01
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-20 14:00:10 -0400

    Oculus platform definition (#57)
    * added initial platform definition for Oculus
    
    * removed
    
    * Updated platform check for Version and check headset connected for runtime. (#59)
    
    Tested on platform
    
    * Added platform overrides
    
    Co-authored-by: Simon (Darkside) Jackson <darkside@zenithmoon.com>

---
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Runtime/OculusPlatform.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Definitions.Platforms;
+using XRTK.Interfaces;
+
+namespace XRTK.Oculus
+{
+    public class OculusPlatform : BasePlatform
+    {
+        private static readonly System.Version NoVersion = new System.Version();
+
+        /// <inheritdoc />
+        public override bool IsAvailable => !UnityEngine.Application.isEditor && OculusApi.Version > NoVersion && OculusApi.Initialized;
+
+        /// <inheritdoc />
+        public override bool IsBuildTargetAvailable
+        {
+            get
+            {
+#if UNITY_EDITOR
+                return (UnityEditor.EditorUserBuildSettings.activeBuildTarget == UnityEditor.BuildTarget.Android ||
+                        UnityEditor.EditorUserBuildSettings.activeBuildTarget == UnityEditor.BuildTarget.StandaloneWindows ||
+                        UnityEditor.EditorUserBuildSettings.activeBuildTarget == UnityEditor.BuildTarget.StandaloneWindows64) &&
+                        OculusApi.Version > NoVersion && OculusApi.Initialized;
+#else
+                return false;
+#endif
+            }
+        }
+
+        /// <inheritdoc />
+        public override IMixedRealityPlatform[] PlatformOverrides { get; } =
+        {
+            new WindowsStandalonePlatform(),
+            new AndroidPlatform()
+        };
+    }
+}
