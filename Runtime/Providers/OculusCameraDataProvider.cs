73f00860e0bebbd4639d80927d8c0b5147b89be3
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-23 18:17:52 -0400

    Camera System Refactor (#54)
    * updated references to camera system
    
    * Changed timing of initialization to prevent race condition
    
    * updated constructors for data providers
    
    * Removed head height settings in controllers
    
    * added initial platform definition for Oculus
    
    * removed
    
    * Added Oculus Camera Data Providers
    
    * updated icons
    
    * Updated platform check for Version and check headset connected for runtime. (#59)
    
    Tested on platform
    
    * Added platform overrides
    
    Co-authored-by: Simon (Darkside) Jackson <darkside@zenithmoon.com>

---
+++ Runtime/Providers/OculusCameraDataProvider.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Definitions.CameraSystem;
+using XRTK.Interfaces.CameraSystem;
+using XRTK.Providers.CameraSystem;
+
+namespace XRTK.Oculus.Providers.CameraSystem
+{
+    public class OculusCameraDataProvider : BaseCameraDataProvider
+    {
+        /// <inheritdoc />
+        public OculusCameraDataProvider(string name, uint priority, BaseMixedRealityCameraDataProviderProfile profile, IMixedRealityCameraSystem parentService)
+            : base(name, priority, profile, parentService)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void ApplySettingsForDefaultHeadHeight()
+        {
+            HeadHeight = OculusApi.EyeHeight;
+            ResetRigTransforms();
+            SyncRigTransforms();
+        }
+    }
+}
