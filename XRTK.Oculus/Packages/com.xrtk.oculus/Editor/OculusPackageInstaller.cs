71da2a438d14cf7dffd53f74a43f9f17743ecfe0
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-12-10 13:19:17 +0000

    Isolated Oculus profiles and prefabs within the Oculus package

--- XRTK.Oculus/Packages/com.xrtk.oculus/Editor/OculusPackageInstaller.cs
+++ XRTK.Oculus/Packages/com.xrtk.oculus/Editor/OculusPackageInstaller.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using XRTK.Editor;
@@ -13,7 +14,13 @@ namespace XRTK.Oculus.Editor
     internal static class OculusPackageInstaller
     {
         private static readonly string DefaultPath = $"{MixedRealityPreferences.ProfileGenerationPath}Oculus";
-        private static readonly string HiddenPath = Path.GetFullPath($"{PathFinderUtility.ResolvePath<IPathFinder>(typeof(OculusPathFinder)).ToForwardSlashes()}\\{MixedRealityPreferences.HIDDEN_PROFILES_PATH}");
+        private static readonly string HiddenProfilePath = Path.GetFullPath($"{PathFinderUtility.ResolvePath<IPathFinder>(typeof(OculusPathFinder)).ToForwardSlashes()}\\{MixedRealityPreferences.HIDDEN_PROFILES_PATH}");
+        private static readonly string HiddenPrefabPath = Path.GetFullPath($"{PathFinderUtility.ResolvePath<IPathFinder>(typeof(OculusPathFinder)).ToForwardSlashes()}\\{MixedRealityPreferences.HIDDEN_PREFABS_PATH}");
+        private static readonly Dictionary<string, string> DefaultOculusAssets = new Dictionary<string, string>
+        {
+            {HiddenProfilePath,  $"{DefaultPath}\\Profiles"},
+            {HiddenPrefabPath, $"{DefaultPath}\\Prefabs"}
+        };
 
         static OculusPackageInstaller()
         {
@@ -23,7 +30,7 @@ namespace XRTK.Oculus.Editor
         [MenuItem("Mixed Reality Toolkit/Packages/Install Oculus Package Assets...", true)]
         private static bool ImportPackageAssetsValidation()
         {
-            return !Directory.Exists($"{DefaultPath}\\Profiles");
+            return !Directory.Exists($"{DefaultPath}\\Profiles") || !Directory.Exists($"{DefaultPath}\\Prefabs");
         }
 
         [MenuItem("Mixed Reality Toolkit/Packages/Install Oculus Package Assets...")]
@@ -37,8 +44,8 @@ namespace XRTK.Oculus.Editor
         {
             if (!EditorPreferences.Get($"{nameof(OculusPackageInstaller)}.Profiles", false))
             {
-                EditorPreferences.Set($"{nameof(OculusPackageInstaller)}.Profiles", PackageInstaller.TryInstallAssets(HiddenPath, $"{DefaultPath}\\Profiles"));
-            }
+                EditorPreferences.Set($"{nameof(OculusPackageInstaller)}.Profiles", PackageInstaller.TryInstallAssets(DefaultOculusAssets));
+            }            
         }
     }
 }
