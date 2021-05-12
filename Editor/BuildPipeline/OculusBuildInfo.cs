8fad3f6afc8993426b2615db4f5345786348f12e
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-11 23:53:19 -0400

    added preproces manifest stubs

--- Editor/BuildPipeline/OculusBuildInfo.cs
+++ Editor/BuildPipeline/OculusBuildInfo.cs
@@ -1,10 +1,14 @@
 // Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Linq;
+using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 using XRTK.Attributes;
 using XRTK.Interfaces;
 using XRTK.Oculus;
+using XRTK.Services;
 
 namespace XRTK.Editor.BuildPipeline
 {
@@ -19,7 +23,16 @@ namespace XRTK.Editor.BuildPipeline
         {
             base.OnPreProcessBuild(report);
 
-            // TODO Generate manifest
+            if (!MixedRealityToolkit.ActivePlatforms.Contains(BuildPlatform) ||
+                EditorUserBuildSettings.activeBuildTarget != BuildTarget)
+            {
+                return;
+            }
+
+            if (BuildPlatform.GetType() == typeof(OculusPlatform))
+            {
+                // TODO generate manifest
+            }
         }
     }
 }
