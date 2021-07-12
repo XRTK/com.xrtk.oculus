f6e8e14e86be2da44cb0fe51d95ce60ba030b8a0
Stephen Hodgson <hodgson.designs@gmail.com>
2021-07-12 09:17:33 -0700

    Untiy 2020 LTS Support (#123)
    * added references to xr sdk (eww)
    
    * updated core dependency
    
    * updated dependencies

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.9")]
+[assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyTitle("com.xrtk.oculus.editor")]
 [assembly: AssemblyCompany("XRTK")]
 [assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
